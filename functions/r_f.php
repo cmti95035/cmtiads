a82e5ccc1a5407b68bd01be64f1bf1e2ccc63eb2
hzmbeyond80 <zhiminhe@gmail.com>
2015-03-03 18:19:07 -0800

    change database config

--- functions/r_f.php
+++ functions/r_f.php
@@ -1098,13 +1098,6 @@ else {
 $query_part['channel']='';
 }
 
-<<<<<<< HEAD
-=======
-//$gender_query="select gender_id from md_mobile_users where phone = '4086803612'";
-//$request_settings['gender'] = simple_query_maindb($qender_query, true, 1)['gender_id'];
-
->>>>>>> 925505d7a0279275bcd9076a14ec7786c95aed86
-//$request_settings['gender'] = '1';
 if (isset($request_settings['gender']) && is_numeric($request_settings['gender'])){
 	$query_part['gender']="AND (md_campaigns.gender_target=1 OR (c2.targeting_type='gender' AND c2.targeting_code='".$request_settings['gender']."'))";
 }
@@ -1540,14 +1533,11 @@ return $result;
 } 
 else 
 {
-	$fp = file_put_contents( '/Users/zhiminhe/logs/cmtiads_internal.log', 'no result');
 return false;
 }
 	
 }
 	
-
-
 
 function get_placement($data){
 global $request_settings;
@@ -1633,23 +1623,22 @@ return false;
 // 
 $pieces = explode("+", $data['s']);
 $request_settings['placement_hash'] = $pieces[0];
-<<<<<<< HEAD
-$gender_query="select gender_id from md_mobile_users where phone = " . $pieces[1];
-$fp = file_put_contents( '/Users/zhiminhe/logs/cmtiads_internal.log', $gender_query, FILE_APPEND);
-$request_settings['gender'] = simple_query_maindb($qender_query, true, 1)['gender_id'];
-$fp = file_put_contents( '/Users/zhiminhe/logs/cmtiads_internal.log', implode(' ', $request_settings['gender']),  FILE_APPEND);
-=======
+
 $request_settings['phone'] = $pieces[1];
 
 $response = file_get_contents($test_config['userinfo_server_url'].$request_settings['phone']);
 
-$fp = file_put_contents($test_config['local_logging_file'] , $response . PHP_EOL .PHP_EOL, FILE_APPEND);
-$userinfo=json_decode($response);
+$fp = file_put_contents($test_config['local_logging_file'] , 
+		$response . PHP_EOL .PHP_EOL, FILE_APPEND);
+$userinfo = json_decode($response, true);
+$fp = file_put_contents( $test_config['local_logging_file'],
+		'userinfo ' . implode(' ',$userinfo) . PHP_EOL .PHP_EOL, FILE_APPEND);
 
-$request_settings['gender'] =$userinfo["gender_id"];
-$fp = file_put_contents( $test_config['local_logging_file'], $request_settings['gender'] . PHP_EOL .PHP_EOL, FILE_APPEND);
+$request_settings['gender'] = $userinfo["gender_id"];
 
->>>>>>> 925505d7a0279275bcd9076a14ec7786c95aed86
+$fp = file_put_contents( $test_config['local_logging_file'], 
+		'gender is ' . $request_settings['gender'] . PHP_EOL .PHP_EOL, FILE_APPEND);
+
 if (!isset($request_settings['placement_hash']) or empty($request_settings['placement_hash']) or !validate_md5($request_settings['placement_hash'])){
 	$errormessage='No valid Integration Placement ID supplied. (Variable "s")';
 	return false;
