a456a63377abf615a42a05a2aa7ac2e84f4daf6e
System Administrator <root@Qs-MacBook-Pro.local>
2015-03-24 11:29:36 -0700

    changed logging file location to /tmp/

--- md.request.php
+++ md.request.php
@@ -6,7 +6,7 @@ require_once 'init.php';
 error_reporting(0);
 
 require_once 'functions/r_f.php';
-$fp = file_put_contents( '/Users/zhiminhe/logs/cmtiads_requests.log', $_SERVER['QUERY_STRING'] . PHP_EOL .PHP_EOL, FILE_APPEND);
+$fp = file_put_contents( '/tmp/cmtiads_requests.log', $_SERVER['QUERY_STRING'] . PHP_EOL .PHP_EOL, FILE_APPEND);
 ad_request($_GET);
 
 ?>
